f923a157b45e2697f0a45147737e76753da9fa8b
“Bhavesh <bhawishdhanjanii@gmail.com>
2023-09-06 16:50:35 +0500

    Learnng Bean Life Cycle

--- src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
+++ src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -4,13 +4,26 @@ import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
+
 public class CricketCoach implements Coach {
 	@Override
 	public String getDailyWorkout() {
 		return "Practise for a hour daily by method";
 		
+	}
+	
+	@PostConstruct
+	public void init() {
+		System.out.println("Making Bean");
+	}
+	
+	@PreDestroy
+	public void destroy() {
+		System.out.println("Destroying the bean");
 	}
 	
 
